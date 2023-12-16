0b7db7a4b7a71567aa6e9790f82152503ec533d3
nagasatya-pedineedi <92015090+nagasatya-pedineedi@users.noreply.github.com>
2023-12-16 17:11:17 +0530

    Add files via upload

---
+++ Basic Matrix input & output.py
@@ -0,0 +1,16 @@
+m, n = map(int, input("Enter m,n: ").split())
+#print(m,n)
+arr = []
+for i in range(m):
+    row = []
+    for j in range(n):
+        value = int(input("Enter {},{} value: ".format(i,j)))
+        row.append(value)
+    arr.append(row)
+print("Printing")
+#for i in arr:
+#    print(i)
+for i in range(m):
+    for j in range(n):
+        print("value in arr[{}][{}]: {}".format(i,j, arr[i][j]))
+    
