27ad897c7a003d5e7da6f2e16980f69301f4cf6e
nagasatya-pedineedi <92015090+nagasatya-pedineedi@users.noreply.github.com>
2023-12-18 13:07:00 +0530

    Add files via upload

---
+++ Matrix Printing Boundary Elements Unordered.py
@@ -0,0 +1,18 @@
+m,n = map(int, input("Enter m n: ").split())
+arr=[]
+for i in range(m):
+    row = []
+    for j in range(n):
+        value = int(input("Enter ({},{}): ".format(i,j)))
+        row.append(value)
+    arr.append(row)
+print("## Array Entered ##")
+print(arr)
+
+result=[]
+for i in range(m):
+    for j in range(n):
+        if i==0 or i==m-1 or j==0 or j==n-1:
+           result.append(arr[i][j])
+print("Result: ", result)
+                    
