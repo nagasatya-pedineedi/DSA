3147344057e426f67b91882bdfc817a8f76ce505
nagasatya-pedineedi <92015090+nagasatya-pedineedi@users.noreply.github.com>
2023-12-22 15:20:41 +0530

    Update Treasure Room 2 Variables.py

--- Treasure Room 2 Variables.py
+++ Treasure Room 2 Variables.py
@@ -2,12 +2,14 @@ n, w = map(int, input().split())
 arr = list(map(int, input().split()))
 
 block_indexes = {}
+flag=False
 for i, block in enumerate(arr):
     diff = w - block
     if diff in block_indexes:
+        flag=True
         print(block_indexes[diff] + 1, i + 1)
         break
     else:
         block_indexes[block] = i
-else:
+if flag==False:
     print(-1)
